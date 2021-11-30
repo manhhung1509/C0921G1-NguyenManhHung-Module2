6264f6852c5d38e52432f6ea12a865abf83adeec
manhhung1509 <hung2332nguyenmanh@gmail.com>
2021-11-30 23:47:27 +0700

    upbai

--- src/case_study/furamaResort/services/impl/FacilityServiceImpl.java
+++ src/case_study/furamaResort/services/impl/FacilityServiceImpl.java
@@ -21,15 +21,15 @@ public class FacilityServiceImpl implements FacilityService {
     Set<Facility> keySet = serviceList.keySet();
 
     static {
-        serviceList.put(new House("house", 100, 5000000, 4, "day", "5 star", 2), 1);
-        serviceList.put(new House("house", 120, 4000000, 5, "year", "4 star", 3), 15);
-        serviceList.put(new House("house", 110, 3000000, 3, "month", "3 star", 3), 3);
-        serviceList.put(new Villa("villa", 110, 20000000, 6, "day", "4 star", 50, 1), 2);
-        serviceList.put(new Villa("villa", 200, 30000000, 7, "year", "5 star", 100, 3), 1);
-        serviceList.put(new Villa("villa", 90, 15000000, 3, "month", "3 star", 50, 2), 4);
-        serviceList.put(new Room("room", 150, 9000000, 7, "hour", "free cocktail"), 7);
-        serviceList.put(new Room("room", 150, 9000000, 7, "hour", "free breakfast"), 10);
-        serviceList.put(new Room("room", 100, 5000000, 4, "year", "massage"), 9);
+        serviceList.put(new House("House", 100, 5000000, 4, "Day", "Good", 2), 1);
+        serviceList.put(new House("House", 120, 4000000, 5, "Year", "Excellent", 3), 15);
+        serviceList.put(new House("House", 110, 3000000, 3, "Month", "Good", 3), 3);
+        serviceList.put(new Villa("Villa", 110, 20000000, 6, "Day", "Good", 50, 1), 2);
+        serviceList.put(new Villa("Villa", 200, 30000000, 7, "Year", "Signature", 100, 3), 1);
+        serviceList.put(new Villa("Villa", 90, 15000000, 3, "Month", "Premium", 50, 2), 4);
+        serviceList.put(new Room("Room", 150, 9000000, 7, "Hour", "free cocktail"), 7);
+        serviceList.put(new Room("Room", 150, 9000000, 7, "Hour", "free breakfast"), 10);
+        serviceList.put(new Room("Room", 100, 5000000, 4, "Hear", "massage"), 9);
     }
 
     @Override
@@ -45,13 +45,13 @@ public class FacilityServiceImpl implements FacilityService {
             choice = Integer.parseInt(scanner.nextLine());
             switch (choice) {
                 case 1:
-                    serviceList.put(addFacility(1), 1);
+                    addFacility(1);
                     break;
                 case 2:
-                    serviceList.put(addFacility(2), 1);
+                    addFacility(2);
                     break;
                 case 3:
-                    serviceList.put(addFacility(3), 1);
+                    addFacility(3);
                     break;
                 case 4:
                     furamaController.displayMainMenu();
@@ -60,7 +60,8 @@ public class FacilityServiceImpl implements FacilityService {
         } while (choice > 4 || choice < 1);
     }
 
-    public Facility addFacility(int choice) {
+    public void addFacility(int choice) {
+        int currentValue = 0;
         String nameService = validate.serviceName();
         double usableArea = validate.usableArea();
         int rentCost = validate.rentCost();
@@ -71,17 +72,32 @@ public class FacilityServiceImpl implements FacilityService {
             Double poolArea = validate.poolArea();
             int floors = validate.numberOfFloors();
             Facility villa = new Villa(nameService, usableArea, rentCost, maximumPeople, rentalType, standard, poolArea, floors);
-            return villa;
+            if (serviceList.containsKey(villa)) {
+                currentValue = serviceList.get(villa) + 1;
+                serviceList.replace(villa, currentValue);
+            } else {
+                serviceList.put(villa, 1);
+            }
         } else if (choice == 2) {
             String standard = validate.standardOfRoom();
             int floors = validate.numberOfFloors();
             Facility house = new House(nameService, usableArea, rentCost, maximumPeople, rentalType, standard, floors);
-            return house;
+            if (serviceList.containsKey(house)) {
+                currentValue = serviceList.get(house) + 1;
+                serviceList.replace(house, currentValue);
+            } else {
+                serviceList.put(house, 1);
+            }
         } else {
             System.out.println("enter free service include: ");
             String freeServiceInclude = scanner.nextLine();
             Facility room = new Room(nameService, usableArea, rentCost, maximumPeople, rentalType, freeServiceInclude);
-            return room;
+            if (serviceList.containsKey(room)) {
+                currentValue = serviceList.get(room) + 1;
+                serviceList.replace(room, currentValue);
+            } else {
+                serviceList.put(room, 1);
+            }
         }
     }
 
